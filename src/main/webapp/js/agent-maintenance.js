73408c150f6b307731b612958759ae22347c32f4
Markus Winter <m.winter@sap.com>
2022-08-28 14:08:44 +0200

    [JENKINS-69455] allow to select all, active or inactive
    When maintenance windows can be removed because the planned activity was
    finished earlier and many maintenance windows exists it is quite painful
    to click all the checkboxes.
    This change will add clickable texts below the table so that one can
    select, all, active inactive and maintenance windows

--- src/main/webapp/js/agent-maintenance.js
+++ src/main/webapp/js/agent-maintenance.js
@@ -16,3 +16,16 @@ function cancelAdd(event) {
         closeForm();
     }
 }
+
+var selectMaintenanceWindows = function(toggle, className) {
+    let table = document.getElementById("maintenance-table")
+    let inputs = table.getElementsByTagName("input");
+    for(var i=0; i<inputs.length; i++) {
+        if(inputs[i].type === "checkbox" && !inputs[i].disabled) {
+            let tr = findAncestor(inputs[i], "TR")
+            if (className == null || tr != null && tr.classList.contains(className)) {
+                inputs[i].checked = toggle;
+            }
+        }
+    }
+};
