7284c08747ebce280bf1bfb8311ee3995dc20abf
prathamalwayscomeslast <way4hell6@gmail.com>
2026-01-06 19:58:47 +0530

    Improved some tests
    Signed-off-by: Pratham Vaghela <prathamcomeslast@gmail.com>

--- src/test/java/com/sap/prd/jenkins/plugins/agent_maintenance/RecurringTest.java
+++ src/test/java/com/sap/prd/jenkins/plugins/agent_maintenance/RecurringTest.java
@@ -2,24 +2,36 @@ package com.sap.prd.jenkins.plugins.agent_maintenance;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.is;
-
 import hudson.model.Slave;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.jvnet.hudson.test.junit.jupiter.WithJenkins;
+
+import java.util.stream.Stream;
 
 /** Test recurring maintenance windows. */
 @WithJenkins
 class RecurringTest extends BaseIntegrationTest {
 
-  @Test
-  void recurringMaintenanceInjectsMaintenance() throws Exception {
-    String agentName = "recurring";
-    Slave agent = getAgent(agentName);
-    MaintenanceTarget target = new MaintenanceTarget(MaintenanceTarget.TargetType.AGENT, agentName);
+  static Stream<MaintenanceTarget.TargetType> allTargets() {
+    return Stream.of(
+            MaintenanceTarget.TargetType.AGENT,
+            MaintenanceTarget.TargetType.CLOUD
+    );
+  }
+
+  @ParameterizedTest(name = "recurringMaintenanceInjectsMaintenance")
+  @MethodSource("allTargets")
+  void recurringMaintenanceInjectsMaintenance(MaintenanceTarget.TargetType targetType) throws Exception {
+    String targetName = "recurring";
+    MaintenanceTarget target = new MaintenanceTarget(targetType, targetName);
     RecurringMaintenanceWindow rw = new RecurringMaintenanceWindow("0 2 * * *",
         "test", true, true, "10m", "60m",  "test", null, 0);
     maintenanceHelper.addRecurringMaintenanceWindow(target.toKey(), rw);
-    triggerCheckCycle(agent);
+    if (targetType == MaintenanceTarget.TargetType.AGENT) {
+      Slave agent = getAgent(targetName);
+      triggerCheckCycle(agent);
+    }
     assertThat(maintenanceHelper.hasMaintenanceWindows(target.toKey()), is(true));
   }
 }
